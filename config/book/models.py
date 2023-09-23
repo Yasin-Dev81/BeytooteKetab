541710897bf8473746d5a4b4061d7cbfb43bf47a
Yasin-Dev81 <eayeay33@yahoo.com>
2023-09-23 14:32:21 +0330

    7cc6860 add inc directory in templates

--- config/book/models.py
+++ config/book/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -14,8 +16,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('home:category_filter', args=[self.slug, ])
+    def get_absolute_url(self):
+        return reverse('book:home')
 
 
 class Book(models.Model):
@@ -24,7 +26,9 @@ class Book(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
     slug = models.SlugField(max_length=200, unique=True)
     description = RichTextField()
-    main_lang = models.CharField(default='en', max_length=3)
+
+    langs = models.ManyToManyField('Lang', related_name='book_langs')
+
     writer = models.CharField(max_length=200)
     image = models.ImageField()
 
@@ -40,3 +44,32 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book:book_detail', args=[self.pk, self.slug, ])
+
+    @property
+    def like_count(self):
+        return self.favorites.count()
+
+
+class Lang(models.Model):
+    lang = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.lang
+
+
+class BookComment(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_comments', blank=True, null=True)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    is_pub = models.BooleanField(default=True)
+
+    sub_comment = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub_comments', null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "comment of %s" % self.author.phone_number
+
+    def get_absolute_url(self):
+        return reverse('book:book_detail', args=[self.book.pk, self.book.slug])
--- config/book/views.py
+++ config/book/views.py
@@ -1,19 +1,54 @@
 from django.shortcuts import render, redirect
-from django.views import generic
-from .models import Book
+from django.views import generic, View
+from .models import Book, Category, BookComment
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 
-def home(request):
-    return render(request, 'book/home.html')
+class HomeView(View):
+    template_name = 'book/home.html'
+    counte_each = 4
+
+    def setup(self, request, *args, **kwargs):
+        self.best_books = sorted(Book.objects.all(), key=lambda t: int(t.like_count)*(-1))[:self.counte_each]
+        self.last_books = Book.objects.all().order_by('-datetime_created')[:self.counte_each*2]
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+            context={
+                'best_book': self.best_books,
+                'last_book': self.last_books
+            }
+        )
 
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(View):
     model = Book
     template_name = 'book/detail.html'
-    pk_url_kwarg = "pk"
+
+    def setup(self, request, *args, **kwargs):
+        self.comments = BookComment.objects.filter(pk=kwargs['pk'])
+        self.book = get_object_or_404(self.model, pk=kwargs['pk'])
+        return super().setup(request, *args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.book.slug == kwargs['slug']:
+            return redirect(reverse('book:book_detail', args=(self.book.pk, self.book.slug, )))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'book': self.book, 'comments': self.comments})
 
 
 class BookListView(generic.ListView):
     model = Book
     template_name = 'book/catalog.html'
     paginate_by = 8
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(BookListView, self).get_context_data(*args, **kwargs)
+        context['category_list'] = Category.objects.all()
+        return context
